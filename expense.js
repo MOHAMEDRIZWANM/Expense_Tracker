69574d5cac2c9a6c74c78d99e953dd2919e91e34
MOHAMEDRIZWANM <101489012+MOHAMEDRIZWANM@users.noreply.github.com>
2024-01-30 11:38:22 +0530

    Delete expense.js

--- expense.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose');
-const expenseSchema=new mongoose.Schema({
-    title:String,
-    amount:Number,
-    desc:String,
-});
-
-const Expense=mongoose.model('Ex',expenseSchema)
-module.exports=Expense;
